90c77ed64cdb5a6e5e5cf7badd45d6dfc4486c80
joohyuneda <68534918+joohyuneda@users.noreply.github.com>
2020-10-13 13:11:36 +0900

    Update and rename 1009motor.ino to motor_spin.ino
    센서에 물체 확인시 좌측으로 회전// 딜레이를 통해 회전 각도 조절

--- car/motor_spin.ino
+++ car/motor_spin.ino
@@ -8,9 +8,7 @@
 int trigerPin = 7;   // 초음파센서 TRIG - D13
 int echoPin = 4;     // 초음파센서 ECHO - D12
 float distance;       // 초음파 센서 거리값 변수
-//int servoPin = 2;     // 서보모터 연결 포트
-int Motor_speed = 100;//모터 속도 PWM
-Servo servo;
+int Motor_speed = 120;//모터 속도 PWM
 
 void forward() {
       digitalWrite(IN1, HIGH);
@@ -51,6 +49,7 @@ void stop() {
       digitalWrite(IN3, LOW);
       digitalWrite(IN4, LOW);
 }
+
 void setup() {
   //모터를 컨트롤하는 출력핀을 설정해줍니다..
   pinMode(ENA,OUTPUT);
@@ -62,10 +61,9 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(trigerPin, OUTPUT); // 초음파 센서 초기화
   pinMode(echoPin, INPUT);  // 초음파 센서 초기화
-//servo.attach(servoPin);   // 서보모터 연결 지정
-//  servo.write(90);          // 초기값으로 정면 응시
   delay(1000);
 }
+
 float getDistanceCM() {   // 초음파 센서 거리 측정 함수(단위:cm)
   digitalWrite(echoPin, LOW);
   digitalWrite(trigerPin, LOW);
@@ -87,32 +85,8 @@ float getStableDistanceCM() {
 void loop() {
    if ( getStableDistanceCM() < 25 ) {   //초음파 센서에서 25cm이하로 장애물 감지
     left();
-    delay(500);
-    
-    //forward();
-//      stop(); 
-//     delay(300);      
-//     servo.write(180); // 서보모터 좌회전 후 측정 거리값 변수에 저장      
-//delay(500);
-//      int leftDistance = getStableDistanceCM();
-//      delay(300);
-//      servo.write(0); // 서보모터 우회전 후 측정 거리값 변수에 저장
-//      delay(500);
-//      int rightDistance = getStableDistanceCM();
-//      delay(300);
-//      servo.write(90);  // 서보모터 중앙으로 원위치
-//      delay(500);
-//      back();           // 0.5초간 후진 
-//      delay(500);  
-//      if (leftDistance > rightDistance) {
-//        left();
-//        } else {
-//        right();
-//      }
-//      delay(500);
-//    } else {          // 장애물 감지가 안 될 경우 전진
-//    forward();
-    }
-//forward();
+    delay(350);//delay값으로 각도 조정 350-400 사이
+    stop();
+    delay(300);
 }
  
