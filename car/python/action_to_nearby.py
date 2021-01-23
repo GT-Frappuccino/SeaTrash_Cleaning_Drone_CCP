52841e30d731dc8d786c230c43ed9c3c0cec9f74
joohyuneda <68534918+joohyuneda@users.noreply.github.com>
2021-01-23 17:26:59 +0900

    Update and rename action.py to action_to_nearby.py

--- car/python/action_to_nearby.py
+++ car/python/action_to_nearby.py
@@ -6,17 +6,16 @@ def action(region):
 
     if (region["back_bl"] > FAR and region["back_left"] > FAR and region["left"] > FAR and region["forward"] > FAR and region["forward_right"] > FAR and           region["right"] > FAR and region["back_right"] > FAR and  region["back_br"] > FAR):
         print("nothing    \n")
-        forward(10)
+        forward(0.01)
     
     elif (direction_min == "forward_left" or direction_min == "left" or direction_min == "back_left" or direction_min == "back_bl"): 
         print("turn left \n")
-        left(10)
+        left(0.01)
     
     elif (direction_min == "forward_right" or direction_min == "right" or direction_min == "back_right" or direction_min == "back_br"):
         print("turn right   \n")
-        right(10)
+        right(0.01)
     
     elif (direction_min == "forward"):
         print("forward   \n")
-        forward(10)
-    
+        forward(0.01)
