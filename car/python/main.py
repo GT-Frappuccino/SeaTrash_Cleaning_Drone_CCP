2ba71f1ae5ef0fa2a1d084a3c84f234c16e477a1
joohyuneda <68534918+joohyuneda@users.noreply.github.com>
2021-01-23 17:30:44 +0900

    Create main.py

---
+++ car/python/main.py
@@ -0,0 +1,24 @@
+ 
+import math
+import rospy
+from sensor_msgs.msg import LaserScan
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+ 
+ 
+def main():
+    try:
+        setup()
+        rospy.init_node('lidar_car')
+    
+        sub = rospy.Subscriber('/scan', LaserScan, clbk_laser)
+    
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        GPIO.cleanup()
+        motorR.stop()
+        motorL.stop()
+if __name__ == '__main__':
+    main()
